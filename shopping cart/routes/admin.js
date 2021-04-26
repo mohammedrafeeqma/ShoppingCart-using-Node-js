edb1c179ad35f74ad98479cb4e0870f033e90a5d
Rafeeq <rafeequemaz@gmail.com>
2021-04-26 12:23:46 +0530

    Data added to admin page and alignment set

--- shopping cart/routes/admin.js
+++ shopping cart/routes/admin.js
@@ -4,38 +4,15 @@ var productHelpers = require("./helpers/product-helpers");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  let products = [
-    {
-      name: "redmi note9",
-      category: "mobile",
-      description: "this is a good phone",
-      image:
-        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRvv7I0H4cdISKBaFO1AkoTBNyvXwh_aTacPDVRqNtDH-DdgkZLVDnglV3yf7mj2tTsRojT4cJ-_X59dv0Ug2sqBQEWdrNdmF84Huhe-KUlFnVWuBrr32e0yw&usqp=CAE",
-    },
-    {
-      name: "iphone",
-      category: "mobile",
-      description: "this is a iphone",
-      image:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRQ8QJPKn-Oo2HI2oeJxfpDKw_vaRiycDfkIFsSOBrAP8Q1gd1GWIlrvd3Gk_NWrifeTfXnxj2uk4Z8WbovtcW2BymYNWtwQDBiD_6JTLa9&usqp=CAE",
-    },
-    {
-      name: "one plus nord",
-      category: "Mobile",
-      description: "this is a one plus nord",
-      image:
-        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTs-FVtxl1ARJXS0srZf7R_h-3uXpAEb6dxEuvO_yzXShKNBol16ZN-VYkGE45PehKvJIpwKVjM7SDu0MBKrqD325vMzJUQ4Q_nWUgawuW3VCsumuQVpmmwFQ&usqp=CAE",
-    },
-    {
-      name: "opoo k3",
-      category: "mobile",
-      description: "this is a oppo k3",
-      image:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQYHDgZGKYC-CgLDOD2XN16qfOxzHv2QGnCXcStbjBqEqGEoHMc2or5ZTq702TQBxuOPAAoG7nwIHp8nh8b7OqyLHQS-OZm&usqp=CAE",
-    },
-  ];
+  //here promise is used
+  productHelpers.getAllProducts().then( (products)=>{
+    console.log(products)
+    res.render("admin/view-products", { admin: true, products });
 
-  res.render("admin/admin-products", { admin: true, products });
+  })
+  
+
+  
 });
 
 // to add Product
@@ -46,10 +23,11 @@ router.get("/add-product", (req, res) => {
 router.post("/add-product", (req, res) => {
   console.log(req.body);
   console.log(req.files.Image); //to got uploaded file
-
+//here callback function is used
   productHelpers.addProduct(req.body, (id) => {
     //it is call function from product-helpers
     let image = req.files.Image;
+    
     //it is for store image in different path
     image.mv("/home/rafeeq/Desktop/Nodejs/project/shopping cart/public/product-images/" +
         id + ".jpg",
--- shopping cart/routes/helpers/product-helpers.js
+++ shopping cart/routes/helpers/product-helpers.js
@@ -1,10 +1,20 @@
 var db=require('/home/rafeeq/Desktop/Nodejs/project/shopping cart/config/connection.js')
+var collection=require("/home/rafeeq/Desktop/Nodejs/project/shopping cart/config/collections.js")
 module.exports={
     addProduct:(product,callback)=>{
         console.log(product)
         db.get().collection('product').insertOne(product).then((data)=>{
             callback(data.ops[0]._id) //id is pass for image file name
         })
+    },
+    //this is the promise function
+    getAllProducts:()=>{
+        return new Promise( async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+
     }
+
     
 }
